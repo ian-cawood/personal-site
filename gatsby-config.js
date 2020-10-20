2c37a4910335c772e93d95896d55a8d1f3e040b4
ian-cawood <ian.cawood@gmail.com>
2020-10-20 21:38:15 +0200

    chore: google analytics and audit updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-180810245-1`,
+        anonymize: true,
+        respectDNT: true,
       },
     },
     `gatsby-plugin-feed`,
