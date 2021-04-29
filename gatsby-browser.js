35cd1ca2cd285921ffe45764cb7669d20fc77cf9
ian-cawood <ian.cawood@gmail.com>
2021-04-29 12:32:33 +0200

    chore: remove old site contents

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import "prismjs/themes/prism.css"
-
-import React from 'react';
-import Layout from './src/components/Layout';
-
-export function wrapPageElement({ element, props }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Layout {...props}>{element}</Layout>;
-}
